0edcdf38634614b29bae88a5f66ee5c3883ec8b9
Gabriel Santos <gvds@ua.pt>
2024-09-28 23:38:07 +0100

    1.4 b) done

---
+++ P1/exe4b/src/main/java/lab1/ex4b/CBD/Autocomplete2.java
@@ -0,0 +1,63 @@
+package lab1.ex4b.CBD;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.ZParams;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Autocomplete2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String prefix = ""; 
+        
+        Jedis jedis = new Jedis();
+        jedis.del("namesByPopularity");
+        jedis.del("namesByAlphabet");
+        
+        try {
+            Scanner fileScanner = new Scanner(new File("../nomes-pt-2021.csv"));
+            String personName;
+            Integer popularityCount;
+            String[] parts;
+
+            while (fileScanner.hasNextLine()) {
+                parts = fileScanner.nextLine().trim().split(";");
+                personName = parts[0];
+                popularityCount = Integer.parseInt(parts[1]);
+                jedis.zadd("namesByPopularity", popularityCount, personName.toLowerCase()); 
+                jedis.zadd("namesByAlphabet", 0, personName.toLowerCase()); 
+            }
+            fileScanner.close();
+            System.out.println("Loading successful");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        while (!prefix.equals("Enter")) {
+            System.out.print("Search for ('Enter' for quit): ");
+            prefix = scanner.nextLine();
+            if (prefix.equals("Enter")) {
+                break;
+            }
+
+            String min = "[" + prefix.toLowerCase(); 
+            String max = min + '\uffff';
+
+            jedis.zrangeByLex("namesByAlphabet", min, max)
+                .forEach((match) -> {
+                    jedis.zadd("matchingNames", 0, match);
+                });
+
+            jedis.zinter(new ZParams(), "namesByPopularity", "matchingNames")
+                .reversed()
+                .forEach(System.out::println);
+
+            jedis.del("matchingNames");
+        }
+
+        jedis.close();
+        scanner.close();
+    }
+}
---
+++ P1/exe4b/src/test/java/lab1/ex4b/CBD/Autocomplete2.java
@@ -0,0 +1,38 @@
+package lab1.ex4b.CBD;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Autocomplete2 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Autocomplete2( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Autocomplete2.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
