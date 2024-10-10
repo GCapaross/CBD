8972960ae63efa0d0ef63b71458b4f81b5585917
gcapaross <gvds@ua.pt>
2024-10-10 11:08:55 +0100

    Updates

--- lab2/src/main/java/com/gabriel/cbd/lab2/ex3/Main.java
+++ lab2/src/main/java/com/gabriel/cbd/lab2/ex3/Main.java
@@ -5,6 +5,10 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import com.gabriel.cbd.lab2.ex3.a.RestaurantA;
+import java.util.Arrays;
+// import java.util.List;
+// Dont forget to start the docker and the mongodb server
 
 public class Main {
     public static void main(String[] args) {
@@ -13,9 +17,21 @@ public class Main {
         MongoDatabase database = mongoClient.getDatabase("cbd");
         MongoCollection<Document> collection = database.getCollection("restaurantes");
 
-        // se houver database e coleçao retorna tudo certo
         System.out.println("Database: " + database.getName());
         System.out.println("Collection: " + collection.getNamespace().getCollectionName());
+
+        RestaurantA restaurantService = new RestaurantA(collection);
+
+        Document address = new Document("building", "123")
+                .append("coord", Arrays.asList(-73.732315, 40.720725))
+                .append("rua", "Jamaica Avenue")
+                .append("zipcode", "11428");
+
+        restaurantService.insertRestaurant("Novo Restaurante", "Brooklyn", "Portuguese", address, 12, 8);
+
+        restaurantService.updateRestaurantName("Novo Restaurante", "Restaurante Atualizado");
+
+        restaurantService.findRestaurantsByLocalidade("Brooklyn");
 
         mongoClient.close();
     }
